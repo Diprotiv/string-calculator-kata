0e56cfa7495c0b2280f07468b8321ad1520fa39d
Diprotiv Sarkar <diprotis@amazon.com>
2020-12-30 16:59:36 +0530

    Adding Maven project and code structure.

---
+++ incubyte-calculator/src/main/java/com/example/incubyte/StringCalculator.java
@@ -0,0 +1,9 @@
+package com.example.incubyte;
+
+/**
+ * Class to demonstrate the functionality of Simple Calculator.
+ */
+public class StringCalculator {
+
+
+}
---
+++ incubyte-calculator/src/test/java/com/example/incubyte/StringCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.example.incubyte;
+
+
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * Class to test the functionality of {@link StringCalculator}
+ */
+public class StringCalculatorTest {
+
+    @BeforeAll
+    public void setup() {
+
+    }
+}
